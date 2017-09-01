a83c41e08e228e67c81765010f8f0a40a359339e
Julien Beugnot <j.beugnot@tenderspage.com>
2017-09-01 18:00:22 +0200

    init commit

---
+++ hotkeys.js
@@ -0,0 +1,17 @@
+module.exports = function(mw){
+          var globalShortcut = require('electron').globalShortcut;
+          var playPause = function(){
+              mw.webContents.executeJavaScript("if(window.parent.frames.playQueue.document.querySelector('#audioPlayer').paused) {window.parent.frames.playQueue.document.querySelector('#audioPlayer').play();}else{window.parent.frames.playQueue.document.querySelector('#audioPlayer').pause();}");
+          };
+          var next = function(){
+              mw.webContents.executeJavaScript("parent.frames.playQueue.onNext();");
+          }
+          var previous = function(){
+              mw.webContents.executeJavaScript("parent.frames.playQueue.onPrevious();");
+          };
+          globalShortcut.register('ctrl+shift+alt+end', playPause);
+          globalShortcut.register('ctrl+shift+option+end', playPause);
+          globalShortcut.register('ctrl+shift+insert', previous);
+          globalShortcut.register('ctrl+shift+home', next);
+};
+
